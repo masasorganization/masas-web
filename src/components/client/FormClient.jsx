a15ce9f22026c7ed413d569b94bfa6e4cbc995bc
muakarada <keymoca@hotmail.com>
2021-11-16 11:47:08 -0500

    Install @mui/lab to date library fix

--- src/components/client/FormClient.jsx
+++ src/components/client/FormClient.jsx
@@ -1,44 +1,73 @@
-import Typography from  '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import Autocomplete from '@mui/material/Autocomplete';
-import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import React from 'react';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 
 function FormClient() {
+    const idclient = [
+        { value: 'Cédula de ciudadanía', label: 'CC'}, 
+        { value: 'Cédula de extranjería', label: 'CE'},
+        { value: 'Tarjeta de identidad', label: 'TI'},
+        { value: 'Pasaporte', label: 'P'},
+    ];
+	const [value, setValue] = React.useState(null);
+    const [id, setId] = React.useState(null);
+    const handleChange = (event) => {
+        setId(event.target.value)
+    };
+
     return(
         <div>
-            <Typography sx={{  mt: '1rem',
-                                ml: '1rem',
-                                fontFamily: 'Nunito, sans-serif',
-                                fontSize: '1.2rem', }}>
-                <Typography sx={{ fontWeight: '700'}} h3>
-                    Formulario de datos
-                </Typography>
-                <Typography sx={{ color: '#c30500' }} h4>
-                    * Campo de datos obligatorio
-                </Typography>
-            </Typography>
-            <Grid spacing={4} container>
-                <Grid item xs={12} md={6}>
-                    <Box sx={{ m: '2rem' }}>
-                        <TextField sx={{ mb: '0.5rem' }} variant="outlined" label="Nombres" required="true" />
-                        <TextField sx={{ mb: '0.5rem' }} variant="outlined" label="Apellidos" required="true" />
-                        <Autocomplete sx={{ mb: '0.5rem' }} disablePortal id="id-client" required="true" options={idclient} renderInput={(params) => <TextField {...params}  label="Tipo de documento" />} />
-                        <TextField variant="outlined" label="Número" required="true" />
-                    </Box>
-                </Grid>
-                <Grid item sx={12} md={6}>
-                    <Box sx={{ m: '2rem' }}>
-                        <TextField sx={{ mb: '0.5rem' }} variant="outlined" label="Dirección" required="true" />
-                        <TextField sx={{ mb: '0.5rem' }} variant="outlined" label="# Casa o apto" />
-                        <TextField sx={{ mb: '0.5rem' }} variant="outlined" label="Barrio" required="true" />
-                        <TextField sx={{ mb: '0.5rem' }} variant="outlined" label="Celular" required="true" />
-                        <TextField variant="outlined" label="Fecha entrega" required="true" />
-                    </Box>
-                </Grid>
-            </Grid>
+            <Box sx={{ display: 'flex', 
+                        flexDirection: { 
+                            md: 'row', 
+                            xs: 'column' } }}>
+                <Box sx={{ m: '2rem', 
+                            width: { 
+                                md: '50%', 
+                                xs: '70%' 
+                            }, 
+                            display: 'flex', 
+                            flexDirection: 'column'}}>
+                    <TextField sx={{ mb: '1rem' }} variant="outlined" label="Nombres" required="true" />
+                    <TextField sx={{ mb: '1rem' }} variant="outlined" label="Apellidos" required="true" />
+                    <TextField sx={{ mb: '1rem' }} select variant="outlined" value={id} onChange={handleChange} label="Tipo de documento" required="true">
+                        {idclient.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))} 
+                    </TextField>
+                    <TextField variant="outlined" label="Número" required="true" />
+                </Box>
+                <Box sx={{ m: '2rem', 
+                            width: { 
+                                md: '50%', 
+                                xs: '70%' 
+                            }, 
+                            display: 'flex', 
+                            flexDirection: 'column' }}>
+                    <TextField sx={{ mb: '1rem' }} variant="outlined" label="Dirección" required="true" />
+                    <TextField sx={{ mb: '1rem' }} variant="outlined" label="# Casa o apto" />
+                    <TextField sx={{ mb: '1rem' }} variant="outlined" label="Barrio" required="true" />
+                    <TextField sx={{ mb: '1rem' }} variant="outlined" label="Celular" required="true" />
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            label="Fecha de entrega"
+                            value={value}
+                            onChange={(newValue) => {
+                            setValue(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                </Box>
+            </Box>
             <div className="btn-ok">
                 <Link to="/deliveryconf">
                     <Button variant="contained" sx={{ bgcolor: '#ff4e00', 
@@ -63,5 +92,3 @@ function FormClient() {
 }
 
 export default FormClient;
-
-const idclient = ['CC', 'CE', 'TI', 'RC', 'PE'];
