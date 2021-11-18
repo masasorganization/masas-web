7b903913e9a261a5f45331e4b651fe3e62741280
Cesar Leon <86802648+IFRaseck@users.noreply.github.com>
2021-11-17 19:17:19 -0500

    interfaz administracion
    desarrollo componentes de la pagina de bienvenida y su interfaz terminada
    desarrollo del componente de gestion para cuenta y producto con sus interfaces terminadas
    actualizacion del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
 import CategoryPage from './pages/admin/welcome';
-import BoxManagement from './components/admin/BoxManagement';
-import ButtonAppBar from './components/admin/NavBarAdmin';
 import ProductManagement from './pages/admin/productmanag';
 import SellerManagement from './pages/admin/sellermanag';
 
@@ -10,7 +8,7 @@ function App() {
     <div className="App">
       {/* <CategoryPage></CategoryPage> */}
       {/* <ProductManagement title='productos'></ProductManagement> */}
-      <SellerManagement title='cuentas'></SellerManagement>
+      {/* <SellerManagement title='cuentas'></SellerManagement> */}
       
     </div>
   );
