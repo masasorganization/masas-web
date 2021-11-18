b671db9982d7e8fb67b0f0a553a291389ed33054
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-11-18 10:09:56 -0500

    navbar grid test

--- src/components/admin/NavBarAdmin.jsx
+++ src/components/admin/NavBarAdmin.jsx
@@ -1,57 +1,54 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Image from '../../assets/isotipo.png';
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Image from '../../assets/isotipo.png'
+import Grid from '@mui/material/Grid'
 
 const ButtonAppBar = function () {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar
-          sx={{
-            bgcolor: '#ffffff',
-            pl: {
-              md: '94px !important',
-              xs: 'none'
-            },
-            pr: {
-              md: '94px !important',
-              xs: '20px'
-            },
-            height: {
-              md: '64px !important',
-              xs: '64px !important'
-            }
-          }}
-        >
+    <Box sx={{ ...navBorder, flexGrow: 1 }}>
+      <AppBar elevation="0" position="static">
+        <Toolbar sx={{ ...toolbarBehaviour }}>
           <IconButton sx={{ display: { xs: 'block', md: 'none' } }}>
             <MenuIcon />
           </IconButton>
           <Box
             sx={{
+              bgcolor: 'red',
               width: '100%',
               display: 'flex',
-              justifyContent: 'space-between',
-              alighContent: 'center'
+              justifyContent: 'space-between'
             }}
           >
-            <Box>
-              <Button variant="text" sx={{ ...buttonStyle }}>
-                Productos
-              </Button>
-              <Button variant="text" sx={{ ...buttonStyle }}>
-                Cuentas
-              </Button>
-              <Button variant="text" sx={{ ...buttonStyle }}>
-                Cifras
-              </Button>
-              <Button variant="text" sx={{ ...buttonStyle }}>
-                Salir
-              </Button>
-            </Box>
+            <Grid
+              spacing={4}
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <Grid item xs={12}>
+                <Button variant="text" sx={{ ...buttonStyle }}>
+                  Productos
+                </Button>
+
+                <Button variant="text" sx={{ ...buttonStyle }}>
+                  Cuentas
+                </Button>
+
+                <Button variant="text" sx={{ ...buttonStyle }}>
+                  Cifras
+                </Button>
+
+                <Button variant="text" sx={{ ...buttonStyle }}>
+                  Salir
+                </Button>
+              </Grid>
+            </Grid>
+            {/* <Box></Box> */}
             <Box className="navBarAdmin">
               <img src={Image} alt="" />
             </Box>
@@ -59,10 +56,10 @@ const ButtonAppBar = function () {
         </Toolbar>
       </AppBar>
     </Box>
-  );
-};
+  )
+}
 
-export default ButtonAppBar;
+export default ButtonAppBar
 
 const buttonStyle = {
   bgcolor: '#ffffff',
@@ -71,4 +68,26 @@ const buttonStyle = {
   fontFamily: 'Noto Sans, sans-serif',
   fontWeight: 'Bold',
   fontSize: '1.25rem'
-};
+}
+
+const navBorder = {
+  borderBottomColor: '#66666699',
+  borderBottomStyle: 'solid',
+  borderBottomWidth: 'thin'
+}
+
+const toolbarBehaviour = {
+  bgcolor: '#ffffff',
+  pl: {
+    md: '94px !important',
+    xs: 'none'
+  },
+  pr: {
+    md: '94px !important',
+    xs: '20px'
+  },
+  height: {
+    md: '64px !important',
+    xs: '64px !important'
+  }
+}
