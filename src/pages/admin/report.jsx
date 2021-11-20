432fd0520a83ec56320f69325a9a5ef102b09d90
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-11-20 10:52:44 -0500

    navbar added

--- src/pages/admin/report.jsx
+++ src/pages/admin/report.jsx
@@ -1,9 +1,12 @@
+import AdminNavbar from '../../components/admin/AdminNavbar'
+
 function Report() {
   return (
     <div>
+      <AdminNavbar estado='1' />
       <h1>Hola vendedor aquí encuentras el informe de tus ventas</h1>
     </div>
-  );
+  )
 }
 
-export default Report;
+export default Report
