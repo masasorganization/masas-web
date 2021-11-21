4bdb8687537e93671303e118db0feac65c18730a
Cesar Leon <86802648+IFRaseck@users.noreply.github.com>
2021-11-21 18:01:26 -0500

    correccion del co

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,81 @@
-import './App.css';
-import CategoryPage from './pages/admin/welcome';
-import ProductManagement from './pages/admin/productmanag';
-import SellerManagement from './pages/admin/sellermanag';
-import SalesPage from './pages/admin/salesrep';
+import './App.css'
+//Se importan las rutas de las vistas para conectar con rutas
+import Home from 'pages/home'
+import Categories from 'pages/client/categories'
+import Products from 'pages/client/products'
+import Orders from 'pages/client/orders'
+import DeliveryRequest from 'pages/client/deliveryreq'
+import DeliveryConfirm from 'pages/client/deliveryconf'
+import Politics from 'pages/client/politics'
+import Login from 'pages/admin/login'
+import Welcome from 'pages/admin/welcome'
+import ProductManagement from 'pages/admin/productmanag'
+import SellerManagement from 'pages/admin/sellermanag'
+import SalesReport from 'pages/admin/salesrep'
+import Report from 'pages/admin/report'
+import OrdersClient from 'pages/admin/ordersclient'
+import Registry from 'pages/admin/registry'
+import Test from 'pages/admin/testfield'
+//Se importa React-Router-Dom
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <CategoryPage></CategoryPage>
-      {/* <ProductManagement title='productos'></ProductManagement> */}
-      {/* <SellerManagement title='cuentas'></SellerManagement> */}
-      {/* <SalesPage title='cifras'></SalesPage> */}
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/politics'>
+            <Politics />
+          </Route>
+          <Route path='/categories'>
+            <Categories />
+          </Route>
+          <Route path='/products'>
+            <Products />
+          </Route>
+          <Route path='/orders'>
+            <Orders />
+          </Route>
+          <Route path='/deliveryreq'>
+            <DeliveryRequest />
+          </Route>
+          <Route path='/deliveryconf'>
+            <DeliveryConfirm />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/productmanag'>
+            <ProductManagement />
+          </Route>
+          <Route path='/sellermanag'>
+            <SellerManagement />
+          </Route>
+          <Route path='/salesrep'>
+            <SalesReport />
+          </Route>
+          <Route path='/report'>
+            <Report />
+          </Route>
+          <Route path='/ordersclient'>
+            <OrdersClient />
+          </Route>
+          <Route path='/registry'>
+            <Registry />
+          </Route>
+          <Route path='/test'>
+            <Test />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
-  
+export default App
--- src/components/client/BtnExit.jsx
+++ src/components/client/BtnExit.jsx
@@ -1,17 +1,132 @@
-import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
+import React from "react";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 function BtnExit() {
-    return(
-        <div>
-            <Button variant="contained" sx={{ bgcolor: '#c30500', 
-                            color: '#ffffff',
-                            textTransform: 'none',
-			                width: '18rem',
-			                fontFamily: 'Noto Sans, sans-serif',
-			                fontSize: '0.9rem'}}>Salir</Button>
-        </div>
+  const [open, setOpen] = React.useState(false);
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
 
-    );
+  return (
+    <div className="btn-client" position="static">
+      <Button
+        onClick={openModal}
+        sx={{
+          bgcolor: "#c30500",
+          color: "#ffffff",
+          borderRadius: "10px",
+          boxShadow: "0",
+          mt: "1rem",
+          textTransform: "none",
+          width: "18rem",
+          fontFamily: "Noto Sans, sans-serif",
+          fontSize: {
+            md: "1.1rem",
+            xs: "1rem",
+          },
+          fontWeight: "700",
+          ":hover": {
+            bgcolor: "#ff823b",
+          },
+        }}
+      >
+        Salir
+      </Button>
+      <Modal
+        open={open}
+        onClose={false}
+        aria-labelledby="title-modal"
+        aria-describedby="description-modal"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: {
+              md: "40%",
+              xs: "70%",
+            },
+            bgcolor: "#ffffff",
+            border: "2px solid #000",
+            p: "2rem",
+          }}
+        >
+          <Typography
+            sx={{ fontWeight: "700", textAlign: "center" }}
+            id="title-modal"
+          >
+            ¿Quieres abandonar el proceso?
+          </Typography>
+          <Typography
+            sx={{ mt: "0.5rem", textAlign: "center", mb: "2rem" }}
+            id="description-modal"
+          >
+            Tu progreso se eliminará
+          </Typography>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <div className="btn-client">
+              <Button
+                onClick={closeModal}
+                sx={{
+                  bgcolor: "#ff4e00",
+                  color: "#ffffff",
+                  borderRadius: "10px",
+                  boxShadow: "0",
+                  mt: "1rem",
+                  mr: {
+                    md: "2rem",
+                    xs: "0",
+                  },
+                  textTransform: "none",
+                  width: "15rem",
+                  fontFamily: "Noto Sans, sans-serif",
+                  fontSize: {
+                    md: "1.1rem",
+                    xs: "1rem",
+                  },
+                  fontWeight: "700",
+                  ":hover": {
+                    bgcolor: "#770047",
+                  },
+                }}
+              >
+                No, quiero continuar
+              </Button>
+              <Link to="/">
+                <Button
+                  sx={{
+                    bgcolor: "#c30500",
+                    color: "#ffffff",
+                    borderRadius: "10px",
+                    boxShadow: "0",
+                    mt: "1rem",
+                    textTransform: "none",
+                    width: "15rem",
+                    fontFamily: "Noto Sans, sans-serif",
+                    fontSize: {
+                      md: "1.1rem",
+                      xs: "1rem",
+                    },
+                    fontWeight: "700",
+                    ":hover": {
+                      bgcolor: "#770047",
+                    },
+                  }}
+                >
+                  Si, ya no me interesa
+                </Button>
+              </Link>
+            </div>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
 
 export default BtnExit;
