76909be2c9690bfefe1e67fcd6c8ab99a605ce63
IFRaseck <leonhurtado22@gmail.com>
2021-11-25 11:35:00 -0500

    commit arreglo de cambios perdidos en el fetch

--- src/pages/admin/welcome.jsx
+++ src/pages/admin/welcome.jsx
@@ -1,7 +1,11 @@
 import { Grid } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/system";
 import { Box } from "@mui/system";
+import { styled } from '@mui/material/styles';
 import BoxCategory from "../../components/admin/BoxCategory";
+
+const Text1Style = styled('p')``;
+const Text2Style = styled('p')``;
 
 const theme = createTheme({
   palette: {
@@ -19,57 +23,20 @@ function CategoryPage() {
     <div>
       <ThemeProvider theme={theme}>
         <div>
-          <Box
-            sx={{
-              color: "negro",
-              textAlign: "Center",
-              letterSpacing: "1.25px",
-              fontSize: "64px",
-              margin: "1.6rem",
-              mb: "0rem",
-              fontFamily: "Nunito",
-              fontWeight: "300",
-              [theme.breakpoints.down("md")]: {
-                fontSize: "28px",
-              },
-            }}
+          <Text1Style
+            sx={{...text1Style}}
           >
             ¿Por dónde quieres
-          </Box>
-          <Box
-            sx={{
-              color: "principalClaro",
-              textAlign: "Center",
-              letterSpacing: "1.25px",
-              fontSize: "64px",
-              margin: "1.6rem",
-              mt: "0rem",
-              fontFamily: "Noto Sans",
-              fontWeight: "bold",
-              [theme.breakpoints.down("md")]: {
-                fontSize: "28px",
-              },
-            }}
+          </Text1Style>
+          <Text2Style
+            sx={{...text2Style}}
           >
             {" "}
             comenzar?
-          </Box>
+          </Text2Style>
         </div>
         <Box
-          sx={{
-            color: "negro",
-            textAlign: "Left",
-            letterSpacing: "1.25px",
-            fontSize: "24px",
-            margin: "1.6rem",
-            mt: "0rem",
-            fontFamily: "Nunito",
-            fontWeight: "300",
-            [theme.breakpoints.down("md")]: {
-              fontSize: "16px",
-              mb: "0rem",
-            },
-          }}
+          sx={{...text3Style}}
         >
           Selecciona una opción:
         </Box>
@@ -121,3 +88,58 @@ function CategoryPage() {
 }
 
 export default CategoryPage;
+
+const text1Style = {
+  color: "negro",
+  textAlign: "Center",
+  letterSpacing: "1.25px",
+  fontSize: "64px",
+  margin: "1.6rem",
+  mb: "0rem",
+  fontFamily: "Nunito",
+  fontWeight: "300",
+  [theme.breakpoints.only("md")]: {
+    fontSize: "48px",
+    mb: "0rem",
+  },
+  [theme.breakpoints.down("md")]: {
+    fontSize: "28px",
+  },
+}
+
+const text2Style = {
+  color: "principalClaro",
+  textAlign: "Center",
+  letterSpacing: "1.25px",
+  fontSize: "64px",
+  margin: "1.6rem",
+  mt: "0rem",
+  fontFamily: "Noto Sans",
+  fontWeight: "bold",
+  [theme.breakpoints.only("md")]: {
+    fontSize: "48px",
+    mb: "0rem",
+  },
+  [theme.breakpoints.down("md")]: {
+    fontSize: "28px",
+  },
+}
+
+const text3Style = {
+  color: "negro",
+  textAlign: "Left",
+  letterSpacing: "1.25px",
+  fontSize: "24px",
+  margin: "1.6rem",
+  mt: "0rem",
+  fontFamily: "Nunito",
+  fontWeight: "300",
+  [theme.breakpoints.only("md")]: {
+    fontSize: "20px",
+    mb: "0rem",
+  },
+  [theme.breakpoints.down("md")]: {
+    fontSize: "16px",
+    mb: "0rem",
+  },
+}
