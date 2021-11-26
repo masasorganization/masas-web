de38fbb5288364ca66018747b83962e0fc87dfd1
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-11-26 01:19:56 -0500

    navigation added

--- src/components/admin/BoxCategory.jsx
+++ src/components/admin/BoxCategory.jsx
@@ -1,87 +1,93 @@
-import { ThemeProvider, createTheme} from '@mui/system';
-import { Box } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/system'
+import { Box } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
-const Text1Style = styled('p')``;
+const Text1Style = styled('p')``
 
 const theme = createTheme({
   palette: {
     background: {
       principalClaro: '#FF823B',
       secundarioClaro: '#AA3D72',
-      azulado: '#05B3B2',
+      azulado: '#05B3B2'
     },
     hover: {
       hoverPrincipalClaro: '#C65306',
       hoverSecundarioClaro: '#770047',
-      hoverAzulado: '#00928E',
+      hoverAzulado: '#00928E'
     },
     text: {
-      white: '#FFFFFF',
-    },
-  },
-});
+      white: '#FFFFFF'
+    }
+  }
+})
 
-const BoxCategory = ({background, hover, title, paragraph, word}) => {
-    return (   
-      <div>
-            <ThemeProvider theme={theme}>
-                <Box sx={{
-                    bgcolor: [background],
-                    m: '20px',
-                    height: '210px',
-                    display: 'inline-flex',
-                    borderRadius: '14px',
-                    [theme.breakpoints.only('md')]: {
-                      height: '250px',
-                    },
-                    [theme.breakpoints.down('md')]: {
-                      height: '150px',
-                      mt: '10px',
-                      mb: '10px',
-                    }, 
-                    ':hover': {
-                      bgcolor: [hover],
-                    },
-                }}>
-                    <div>
-                        <Box sx={{ 
-                            color: "text.white",
-                            textAlign: 'left',
-                            letterSpacing: '1.25px',
-                            fontSize: '32px', 
-                            margin: '1.6rem',
-                            fontFamily: 'Noto Sans',
-                            fontWeight: 'bold',
-                            [theme.breakpoints.down('md')]: {
-                              margin: '1.3rem',
-                              mb: '0.313rem',
-                              mt: '0.938rem',
-                            },
-                            }}>
-                                {[title]}
-                        </Box>
-                        <Text1Style sx={{ 
-                            color: "text.white",
-                            textAlign: 'right',
-                            letterSpacing: '1.25px',
-                            fontSize: '20px', 
-                            m: '1.6rem',
-                            fontFamily: 'Nunito',
-                            [theme.breakpoints.down('md')]: {
-                              fontSize: '18px',
-                              margin: '1.3rem',
-                              mt: '0rem',
-                            },
-                            }}>
-                                {paragraph}{word}
-                        </Text1Style>           
-                    </div>
-                </Box>
-            </ThemeProvider>
-      </div>
-    )
-        
+const BoxCategory = ({ background, hover, title, paragraph, word }) => {
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            bgcolor: [background],
+            m: '20px',
+            height: '210px',
+            display: 'inline-flex',
+            borderRadius: { xs: '7px', md: '14px' },
+            [theme.breakpoints.only('md')]: {
+              height: '250px'
+            },
+            [theme.breakpoints.down('md')]: {
+              height: '150px',
+              mt: '10px',
+              mb: '10px'
+            },
+            ':hover': {
+              bgcolor: [hover]
+            }
+          }}
+        >
+          <div>
+            <Box
+              sx={{
+                color: 'text.white',
+                textAlign: 'left',
+                letterSpacing: '1.25px',
+                fontSize: '32px',
+                margin: '1.6rem',
+                fontFamily: 'Noto Sans',
+                fontWeight: 'bold',
+                [theme.breakpoints.down('md')]: {
+                  margin: '1.3rem',
+                  mb: '0.313rem',
+                  mt: '0.938rem'
+                }
+              }}
+            >
+              {[title]}
+            </Box>
+            <Text1Style
+              sx={{
+                color: 'text.white',
+                textAlign: 'right',
+                letterSpacing: '1.25px',
+                fontSize: '20px',
+                m: '1.6rem',
+                fontFamily: 'Nunito',
+                [theme.breakpoints.down('md')]: {
+                  fontSize: '18px',
+                  margin: '1.3rem',
+                  mt: '0rem'
+                }
+              }}
+            >
+              {paragraph}
+              {word}
+            </Text1Style>
+          </div>
+        </Box>
+      </ThemeProvider>
+    </div>
+  )
 }
 
-export default BoxCategory;
+export default BoxCategory
--- src/pages/admin/welcome.jsx
+++ src/pages/admin/welcome.jsx
@@ -1,88 +1,99 @@
-import { Grid } from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/system";
-import { Box } from "@mui/system";
-import { styled } from '@mui/material/styles';
-import BoxCategory from "../../components/admin/BoxCategory";
+import { Grid } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/system'
+import { Box } from '@mui/system'
+import { styled } from '@mui/material/styles'
+import BoxCategory from '../../components/admin/BoxCategory'
+import { Link } from 'react-router-dom'
 
-const Text1Style = styled('p')``;
-const Text2Style = styled('p')``;
+const Text1Style = styled('p')``
+const Text2Style = styled('p')``
 
 const theme = createTheme({
   palette: {
-    blanco: "#FFFFFF",
-    negro: "#000000",
-    gris: "#dadada",
-    principalClaro: "#FF823B",
-    secundarioClaro: "#AA3D72",
-    azulado: "#05B3B2",
-  },
-});
+    blanco: '#FFFFFF',
+    negro: '#000000',
+    gris: '#dadada',
+    principalClaro: '#FF823B',
+    secundarioClaro: '#AA3D72',
+    azulado: '#05B3B2'
+  }
+})
 
 function CategoryPage() {
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <div>
-          <Text1Style
-            sx={{...text1Style}}
-          >
-            ¿Por dónde quieres
-          </Text1Style>
-          <Text2Style
-            sx={{...text2Style}}
-          >
-            {" "}
-            comenzar?
-          </Text2Style>
-        </div>
         <Box
-          sx={{...text3Style}}
+          sx={{
+            height: { xs: '184px', md: '295px' },
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center'
+            //my: '20px'
+          }}
         >
-          Selecciona una opción:
+          <Text1Style sx={{ ...text1Style }}>¿Por dónde quieres</Text1Style>
+          <Text2Style sx={{ ...text2Style }}> comenzar?</Text2Style>
         </Box>
-        <Grid container justifyContent={"center"}>
+        <Box sx={{ ...text3Style }}>Selecciona una opción:</Box>
+        <Grid container justifyContent={'center'}>
           <Grid item xs={12} sm={11} md={4}>
-            <BoxCategory
-              background="background.principalClaro"
-              hover="hover.hoverPrincipalClaro"
-              title="Productos"
-              paragraph="En esta sección puedes crear, editar, modificar y archivar los productos del "
-              word= "catalogo."
-            />{" "}
+            <Box component={Link} to={'productmanag'}>
+              <BoxCategory
+                background='background.principalClaro'
+                hover='hover.hoverPrincipalClaro'
+                title='Productos'
+                paragraph='En esta sección puedes crear, editar, modificar y archivar los productos del '
+                word='catalogo.'
+              />
+            </Box>
           </Grid>
           <Grid item xs={12} sm={11} md={4}>
-            <BoxCategory
-              background="background.secundarioClaro"
-              hover="hover.hoverSecundarioClaro"
-              title="Cuentas"
-              paragraph="En esta sección puedes crear, editar, modificar y eliminar las cuantas de "
-              word= "usuario."
-            />
+            <Box component={Link} to={'sellermanag'}>
+              <BoxCategory
+                background='background.secundarioClaro'
+                hover='hover.hoverSecundarioClaro'
+                title='Cuentas'
+                paragraph='En esta sección puedes crear, editar, modificar y eliminar las cuantas de '
+                word='usuario.'
+              />
+            </Box>
           </Grid>
           <Grid item xs={12} sm={11} md={4}>
-            <BoxCategory
-              background="background.azulado"
-              hover="hover.hoverAzulado"
-              title="Cifras"
-              paragraph="En esta sección puedes acceder a los datos que se han recolectado sobre productos y "
-              word= "ventas."
-            />
+            <Box component={Link} to={'salesrep'}>
+              <BoxCategory
+                background='background.azulado'
+                hover='hover.hoverAzulado'
+                title='Cifras'
+                paragraph='En esta sección puedes acceder a los datos que se han recolectado sobre productos y '
+                word='ventas.'
+              />
+            </Box>
           </Grid>
           <Grid item xs={12} sm={11} md={4}>
             <Box
+              component={Link}
+              to={'login'}
               sx={{
-                bgcolor: "gris",
-                color: "blanco",
-                height: "60px",
-                display: "flex",
-                m: "20px",
-                borderRadius: 3,
-                fontFamily: "Noto Sans",
-                fontSize: "24px",
-                letterSpacing: "1.25px",
-                alignItems: "center",
-                justifyContent: "center",
-                [theme.breakpoints.down('md')]: {height: '30px', fontSize: '18px'}
+                bgcolor: 'gris',
+                color: 'blanco',
+                height: '60px',
+                display: 'flex',
+                m: '20px',
+                borderRadius: 2,
+                fontFamily: 'Noto Sans',
+                fontSize: '24px',
+                letterSpacing: '1.25px',
+                alignItems: 'center',
+                justifyContent: 'center',
+                textDecoration: 'none',
+                [theme.breakpoints.down('md')]: {
+                  height: '30px',
+                  fontSize: '18px'
+                },
+                ':hover': {
+                  backgroundColor: '#979797'
+                }
               }}
             >
               Salir
@@ -91,62 +102,62 @@ function CategoryPage() {
         </Grid>
       </ThemeProvider>
     </div>
-  );
+  )
 }
 
-export default CategoryPage;
+export default CategoryPage
 
 const text1Style = {
-  color: "negro",
-  textAlign: "Center",
-  letterSpacing: "1.25px",
-  fontSize: "64px",
-  margin: "1.6rem",
-  mb: "0rem",
-  fontFamily: "Nunito",
-  fontWeight: "300",
-  [theme.breakpoints.only("md")]: {
-    fontSize: "48px",
-    mb: "0rem",
+  color: 'negro',
+  textAlign: 'Center',
+  letterSpacing: '1.25px',
+  fontSize: '60px',
+  margin: '1.6rem',
+  mb: '0rem',
+  fontFamily: 'Nunito',
+  fontWeight: '300',
+  [theme.breakpoints.only('md')]: {
+    fontSize: '48px',
+    mb: '0rem'
   },
-  [theme.breakpoints.down("md")]: {
-    fontSize: "28px",
-  },
+  [theme.breakpoints.down('md')]: {
+    fontSize: '32px'
+  }
 }
 
 const text2Style = {
-  color: "principalClaro",
-  textAlign: "Center",
-  letterSpacing: "1.25px",
-  fontSize: "64px",
-  margin: "1.6rem",
-  mt: "0rem",
-  fontFamily: "Noto Sans",
-  fontWeight: "bold",
-  [theme.breakpoints.only("md")]: {
-    fontSize: "48px",
-    mb: "0rem",
+  color: 'principalClaro',
+  textAlign: 'Center',
+  letterSpacing: '1.25px',
+  fontSize: '60px',
+  margin: '1.6rem',
+  mt: '0rem',
+  fontFamily: 'Noto Sans',
+  fontWeight: 'bold',
+  [theme.breakpoints.only('md')]: {
+    fontSize: '48px',
+    mb: '0rem'
   },
-  [theme.breakpoints.down("md")]: {
-    fontSize: "28px",
-  },
+  [theme.breakpoints.down('md')]: {
+    fontSize: '32px'
+  }
 }
 
 const text3Style = {
-  color: "negro",
-  textAlign: "Left",
-  letterSpacing: "1.25px",
-  fontSize: "24px",
-  margin: "1.6rem",
-  mt: "0rem",
-  fontFamily: "Nunito",
-  fontWeight: "300",
-  [theme.breakpoints.only("md")]: {
-    fontSize: "20px",
-    mb: "0rem",
+  color: 'negro',
+  textAlign: 'Left',
+  letterSpacing: '1.25px',
+  fontSize: '24px',
+  margin: '1.6rem',
+  mt: '0rem',
+  fontFamily: 'Nunito',
+  fontWeight: '300',
+  [theme.breakpoints.only('md')]: {
+    fontSize: '20px',
+    mb: '0rem'
   },
-  [theme.breakpoints.down("md")]: {
-    fontSize: "16px",
-    mb: "0rem",
-  },
+  [theme.breakpoints.down('md')]: {
+    fontSize: '16px',
+    mb: '0rem'
+  }
 }
