bd4026559f7b13d862fd5c168b59f9a35d48beb6
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-11-30 16:41:23 -0500

    Style fixed

--- src/pages/client/products.jsx
+++ src/pages/client/products.jsx
@@ -1,15 +1,15 @@
-import Grid from "@mui/material/Grid";
-import Tab from "@mui/material/Tab";
-import Tabs from '@mui/material/Tabs';
-import AppBar from '@mui/material/AppBar';
-import BtnExit from "../../components/client/BtnExit";
-import CardProduct from "../../components/client/CardProduct";
-import React from 'react';
+import Grid from '@mui/material/Grid'
+import Tab from '@mui/material/Tab'
+import Tabs from '@mui/material/Tabs'
+import AppBar from '@mui/material/AppBar'
+import BtnExit from '../../components/client/BtnExit'
+import CardProduct from '../../components/client/CardProduct'
+import React from 'react'
 
 function Products(props) {
-  const { match, history } = props;
-  const { params } = match;
-  const { category } = params;
+  const { match, history } = props
+  const { params } = match
+  const { category } = params
 
   const tabIndex = {
     0: 'sugarfree',
@@ -18,58 +18,116 @@ function Products(props) {
   }
 
   const indexTab = {
-    'sugarfree': 0,
-    'healthydough': 1,
-    'organic': 2
+    sugarfree: 0,
+    healthydough: 1,
+    organic: 2
   }
 
-  const [selTab, setSelTab] = React.useState(indexTab[category]);
+  const [selTab, setSelTab] = React.useState(indexTab[category])
   const clickTab = (event, newValue) => {
-    history.push(`/products/${tabIndex[newValue]}`);
+    history.push(`/products/${tabIndex[newValue]}`)
     setSelTab(newValue)
-  };
+  }
+
+  const dataSinAzucar = [
+    {
+      _id: '61a69676bbefa20b236bd1f3',
+      fechaCreacion: '2021-11-30T00:00:00.000Z',
+      categoria: 'Sin azúcar',
+      nombre: 'Torta de chocolate amargo',
+      porciones: 8,
+      precio: 20000,
+      descripcion: 'Torta elaborada con chocolate amargo y leche de almendras',
+      ingredientes:
+        'Harina, sal, mantequilla orgánica, chocolate amargo, leche de almendras',
+      __v: 0
+    }
+  ]
+
+  const dataMasaSaludable = [
+    {
+      _id: '6195becd3ffb016276b89674',
+      fechaCreacion: '2021-11-14T00:00:00.000Z',
+      categoria: 'Masas saludables',
+      nombre: 'Torta de Almendras',
+      porciones: 8,
+      precio: 40000,
+      descripcion: 'Torta elaborada con harina de almendras',
+      ingredientes: 'Harina, sal, panela, almendras, agua',
+      __v: 0
+    },
+    {
+      _id: '6195caf53ffb016276b89693',
+      fechaCreacion: '2021-11-14T00:00:00.000Z',
+      categoria: 'Masas saludables',
+      nombre: 'Torta de Quinua blanca',
+      porciones: 8,
+      precio: 32000,
+      descripcion: 'Torta elaborada con quinua blanca',
+      ingredientes: 'Harina, sal, panela, quinua, agua',
+      __v: 0
+    }
+  ]
+
+  const dataOrganico = [
+    {
+      _id: '61a695d2bbefa20b236bd1f2',
+      fechaCreacion: '2021-11-30T00:00:00.000Z',
+      categoria: 'Orgánico',
+      nombre: 'Torta de naranja y zanahoria',
+      porciones: 8,
+      precio: 40000,
+      descripcion: 'Torta elaborada con naranja y zanahoria',
+      ingredientes: 'Harina, sal, panela, naranja, zanahoria, aceite',
+      __v: 0
+    }
+  ]
 
   return (
     <div>
       <Grid container>
-        <AppBar sx={{ bgcolor: '#ff4e00',
-                      boxShadow: '0' }} position="static">
-          <Tabs sx={{ height: '3.5rem' }} variant="fullWidth" value={selTab} onChange={clickTab}>
-              <Tab sx={{ ...tabStyle }} label="Sin azúcar" />
-              <Tab sx={{ ...tabStyle }} label="Masa saludable" />
-              <Tab sx={{ ...tabStyle }} label="Orgánico" />
+        <AppBar sx={{ bgcolor: '#ff4e00', boxShadow: '0' }} position='static'>
+          <Tabs
+            sx={{ height: '3.5rem' }}
+            variant='fullWidth'
+            value={selTab}
+            onChange={clickTab}
+          >
+            <Tab sx={{ ...tabStyle }} label='Sin azúcar' />
+            <Tab sx={{ ...tabStyle }} label='Masa saludable' />
+            <Tab sx={{ ...tabStyle }} label='Orgánico' />
           </Tabs>
         </AppBar>
-        {  selTab === 0 && <Sugarfree />}
-        {  selTab === 1 && <HealthyDough />}
-        {  selTab === 2 && <Organic />}
+        {selTab === 0 && <Sugarfree />}
+        {selTab === 1 && <HealthyDough />}
+        {selTab === 2 && <Organic />}
         <Grid item xs={12} md={12}>
           <BtnExit />
         </Grid>
       </Grid>
     </div>
-  );
+  )
 }
 
-export default Products;
+export default Products
 
 const tabStyle = {
-  color: "#ffffff",
+  color: '#ffffff',
   fontSize: {
-    md: "1.2rem",
-    xs: "0.9rem",
+    md: '1.2rem',
+    xs: '0.9rem'
   },
-  ":hover": {
+  ':hover': {
     bgcolor: '#770047',
     color: '#ffffff'
   },
-  fontFamily: "Noto Sans, sans-serif",
-  fontWeight: "700",
-  textTransform: "none",
+  fontFamily: 'Noto Sans, sans-serif',
+  fontWeight: '700',
+  textTransform: 'none'
 }
 
 function Sugarfree() {
-  return(
+  return (
     <>
       <CardProduct />
       <CardProduct />
@@ -78,7 +136,7 @@ function Sugarfree() {
 }
 
 function HealthyDough() {
-  return(
+  return (
     <>
       <CardProduct />
       <CardProduct />
@@ -88,11 +146,9 @@ function HealthyDough() {
 }
 
 function Organic() {
-  return(
+  return (
     <>
       <CardProduct />
     </>
   )
 }
-
-
