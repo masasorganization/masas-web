7d27c0e040454ad8ee97a1d853d2f0673cfb4664
afar-cmyk <andresfelipe.alvarez03@gmail.com>
2021-12-07 15:11:06 -0600

    minor bugfix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import Axios from 'axios'
 
 Axios.defaults.baseURL = 'https://masasapp.herokuapp.com/'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
